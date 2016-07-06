56e728f3d930b233ce4a5a6b66217bd6956d8b20
Evgeny Palyanov <i-fly@mail.ru>
2016-07-06 18:16:45 +0300

    create class and method working with name

---
+++ example_user.rb
@@ -0,0 +1,22 @@
+class User
+  attr_accessor :name, :email
+
+  def initialize(attributes = {})
+    @name  = attributes[:name]
+    @email = attributes[:email]
+  end
+
+  def formatted_email
+    "#{@name} <#{@email}>"
+  end
+
+  def full_name
+    @arr = @name.split(" ")
+    "#{@arr[0]} #{@arr[1]}"
+  end
+
+  def alphabetical_name
+    @arr = @name.split(" ")
+    "#{@arr[1]}, #{@arr[0]}"
+  end
+end
