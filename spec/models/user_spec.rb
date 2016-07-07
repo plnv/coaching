f7e6e061293b3b1456a70d82179c7edd1c05e23b
Evgeny Palyanov <i-fly@mail.ru>
2016-07-07 18:15:14 +0300

    change shoud to expect

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,42 +7,37 @@ describe User do
 
   subject { @user }
 
-  it { should respond_to(:name) }
-  it { should respond_to(:email) }
-  it { should respond_to(:password_digest) }
-  it { should respond_to(:password) }
-  it { should respond_to(:password_confirmation) }
+  it { is_expected.to respond_to(:name) }
+  it { is_expected.to respond_to(:email) }
+  it { is_expected.to respond_to(:password_digest) }
+  it { is_expected.to respond_to(:password) }
+  it { is_expected.to respond_to(:password_confirmation) }
 
 
 
-  describe "should be valid" do
-    it { should be_valid }
+  describe "expected to be valid" do
+    it { is_expected.to be_valid }
   end
 
-  describe "name should be present" do
+  describe "name expected to be present" do
     before { @user.name = " " }
-    it { should_not be_valid }
+    it { is_expected.not_to be_valid }
   end
 
   describe "name should not be too long" do
     before { @user.name = "a" * 51 }
-    it { should_not be_valid }
+    it { is_expected.not_to be_valid }
   end
 
 
 
   describe "email should not be too long" do
     before { @user.name = "a" * 244 + "@example.com" }
-    it { should_not be_valid }
-  end
-
-  describe "email should be present" do
-    before { @user.email = " " }
-    it { should_not be_valid }
+    it { is_expected.not_to be_valid }
   end
 
   describe "when email format is invalid" do
-    it "should be invalid" do
+    it "expect to be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com user@..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
@@ -52,7 +47,7 @@ describe User do
   end
 
   describe "when email format is valid" do
-    it "should be valid" do
+    it "expect to be valid" do
       addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
       addresses.each do |valid_address|
         @user.email = valid_address
@@ -63,14 +58,14 @@ describe User do
 
 
 
-  describe "password should be present (nonblank)" do
+  describe "password expected to be present (nonblank)" do
     before { @user.password = @user.password_confirmation = " " * 6 }
-    it { should be_invalid }
+    it { is_expected.to be_invalid }
   end
 
   describe "password should have a minimum length" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
-    it { should be_invalid }
+    it { is_expected.to be_invalid }
   end
 
   describe "return value of authenticate method" do
@@ -78,7 +73,7 @@ describe User do
     let(:found_user) { User.find_by(email: @user.email) }
 
     describe "with valid password" do
-      it { should eq found_user.authenticate(@user.password) }
+      it { is_expected.to eq found_user.authenticate(@user.password) }
     end
   end
 end
