b39551ce580b4b259117dbae0b8c14cbd0ce4e0d
Evgeny Palyanov <i-fly@mail.ru>
2016-07-08 17:29:18 +0300

    Create pages for registration new users and show user information. Add rspec test for that.

--- spec/features/users_pages_spec.rb
+++ spec/features/users_pages_spec.rb
@@ -3,17 +3,62 @@ require 'rails_helper'
 
 describe "Users pages" do
 
+  subject { page }
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { is_expected.to have_content(user.name) }
+    it { is_expected.to have_title(user.name) }
+  end
+
   describe "Sign up page" do
 
+    before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { is_expected.to have_title('Sign up') }
+        it { is_expected.to have_content('error') }
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name", with: "Example User"
+        fill_in "Email", with: "user@example.com"
+        fill_in "Password", with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "expect create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { is_expected.to have_title(user.name) }
+        it { is_expected.to have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+    end
+
     it "should have the content 'Sign up'" do
-      visit signup_path
       expect(page).to have_content('Sign up')
     end
 
     it "should have the title 'Sign up'" do
-      visit signup_path
       expect(page).to have_title("Sign up | Ruby on Rails Tutorial Sample App")
     end
   end
-
 end
