14a8147cde26f33b89c4727f5e249fe0255b5843
Evgeny Palyanov <i-fly@mail.ru>
2016-07-12 16:27:27 +0300

    create multiple user for Factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
-  factory :user do
-    name "Michael Hartl"
-    email "michael@example.com"
-    password "foobar"
-    password_confirmation "foobar"
+  factory :user do |u|
+    u.sequence(:name) { |n| "Michael Hartl#{n}" }
+    u.sequence(:email) { |n| "michael#{n}@example.com" }
+    u.password "foobar"
+    u.password_confirmation { |u| u.password }
   end
 end
