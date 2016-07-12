5634d7afc9f17bed87a152b2b1618cb2d893c5b7
Evgeny Palyanov <i-fly@mail.ru>
2016-07-12 18:34:29 +0300

    add tests

--- spec/features/sessions_helper_spec.rb
+++ spec/features/sessions_helper_spec.rb
@@ -1,13 +1,46 @@
 require 'rails_helper'
 
-describe "Authentication" do
+
+describe 'Authentication' do
 
   subject { page }
+  let(:login) {'Log in'}
 
-  describe "signin page" do
+  describe 'signin page' do
     before { visit login_path }
 
     it { is_expected.to have_content('Log in') }
     it { is_expected.to have_title('Log in') }
   end
+
+  describe 'signin' do
+    before { visit login_path }
+
+    describe 'with invalid information' do
+      before { click_button login }
+
+      it { is_expected.to have_title('Log in') }
+      it { is_expected.to have_selector('div.alert.alert-danger') }
+
+      describe 'after visiting another page' do
+        before { click_link 'Home' }
+        it { is_expected.to have_no_selector('div.alert.alert-danger') }
+      end
+    end
+
+    describe 'with valid information' do
+      let(:user) { FactoryGirl.create(:user) }
+
+      before do
+        fill_in 'Email', with: user.email
+        fill_in 'Password', with: user.password
+        click_button login
+      end
+
+      it { is_expected.to have_title(user.name) }
+      it { is_expected.to have_link('Profile', href: user_path(user)) }
+      it { is_expected.to have_link('Log out', href: logout_path) }
+    end
+  end
+  
 end
