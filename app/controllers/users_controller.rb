9ddff3254634c261de8b757ea0f7ca35da04f0dd
Evgeny Palyanov <i-fly@mail.ru>
2016-07-15 17:10:26 +0300

    login protection and test for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -31,8 +35,27 @@ class UsersController < ApplicationController
     end
   end
 
+  # Before filters
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
---
+++ spec/features/users_edit_spec.rb
@@ -0,0 +1,76 @@
+require 'rails_helper'
+
+
+describe 'User edit profile' do
+
+  subject { page }
+  let(:user) { FactoryGirl.create(:user, {name: 'Michael Hartl', email: 'michael@example.com', password: 'foobar'}) }
+  let(:new_user) { FactoryGirl.create(:user, {name: 'New User', email: 'newuser@mail.com', password: 'barfoo'}) }
+
+  describe 'with login' do
+    before {
+      visit login_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Log in'
+      visit edit_user_path(user)
+    }
+
+    it 'expect to have' do
+      is_expected.to have_content('Update your profile')
+      is_expected.to have_title('Edit user')
+      is_expected.to have_link('change', href: 'http://gravatar.com/emails')
+    end
+
+    it 'as invalid information' do
+      fill_in 'Email', with: ''
+      click_button 'Save changes'
+
+      is_expected.to have_content('error')
+    end
+
+    it 'as valid information' do
+      fill_in 'Name', with: "New User"
+      fill_in 'Email', with: "new@user.com"
+      click_button 'Save changes'
+
+      is_expected.to have_title("New User")
+      is_expected.to have_selector('div.alert.alert-success')
+      is_expected.to have_link('Log out', href: logout_path)
+      expect(user.reload.name).to eq "New User"
+      expect(user.reload.email).to eq "new@user.com"
+    end
+  end
+
+  describe 'without log in' do
+    it 'expect redirect to log in' do
+      visit edit_user_path(user)
+
+      is_expected.to have_title('Log in')
+      is_expected.to have_selector('div.alert.alert-danger')
+    end
+  end
+
+  describe "with different user" do
+    it 'expect redirect to root' do
+      visit login_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Log in'
+      visit edit_user_path(new_user)
+
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+  end
+
+  describe "return to edit" do
+    it 'after login' do
+      visit edit_user_path(user)
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Log in'
+
+      is_expected.to have_title('Edit user')
+    end
+  end
+end
