bc7cedcaf75271392108a870a519795dc697f5de
Evgeny Palyanov <i-fly@mail.ru>
2016-07-20 17:50:03 +0300

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,8 +7,8 @@ Rails.application.configure do
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => ENV['app53009701@heroku.com'],
-      :password       => ENV['z3weyzzs7807'],
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
       :domain         => 'heroku.com',
       :enable_starttls_auto => true
   }
