6594723764b46998fbdedf4edd120598c10b7e46
Evgeny Palyanov <i-fly@mail.ru>
2016-07-22 18:22:31 +0300

    micropost refactoring, show on home page

---
+++ spec/features/micropost_pages_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe "Micropost pages" do
+
+  subject { page }
+
+  let(:user) { FactoryGirl.create(:user, {name: 'Michael Hartl', email: 'michael@example.com', password: "foobar", activated:true}) }
+
+  before do
+    visit login_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+  end
+
+  describe "micropost creation" do
+    before { visit root_path }
+
+    describe "with invalid information" do
+
+      it "should not create a micropost" do
+        expect { click_button "Post" }.not_to change(Micropost, :count)
+      end
+
+      describe "error messages" do
+        before { click_button "Post" }
+        it { is_expected.to have_content('error') }
+      end
+    end
+
+    describe "with valid information" do
+
+      before { fill_in 'micropost_content', with: "Lorem ipsum" }
+      it "should create a micropost" do
+        expect { click_button "Post" }.to change(Micropost, :count).by(1)
+      end
+    end
+  end
+end
